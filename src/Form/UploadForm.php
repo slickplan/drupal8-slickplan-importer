f88c5adf4e148e42cc394db730cba66e92d8069e
lukaszawmous <lukasz@awmo.us>
2017-06-06 18:52:04 +0200

    Configure a read uploaded file, and init proceed a content file.

--- src/Form/UploadForm.php
+++ src/Form/UploadForm.php
@@ -1,8 +1,10 @@
 <?php
 namespace Drupal\slickplan\Form;
 
+use Drupal\file\Entity\File;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\slickplan\Controller\SlickplanController;
 
 class UploadForm extends FormBase
 {
@@ -15,16 +17,21 @@ class UploadForm extends FormBase
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $file_validator = array(
-            'file_validate_extensions'=>array('xml'),
-            'file_validate_size'=>array(file_upload_max_size())
+            'file_validate_extensions' => array(
+                'xml'
+            ),
+            'file_validate_size' => array(
+                file_upload_max_size()
+            )
         );
         
         $form['slickplan'] = array(
-            '#type' => 'file',
+            '#type' => 'managed_file',
+            '#upload_location' => 'public://upload/slickplan',
             '#title' => 'Slickplan Importer',
             '#required' => TRUE,
             '#description' => '<p>The Slickplan Importer plugin allows you to quickly import your ' . '<a href="http://slickplan.com" target="_blank">Slickplan</a> projects into your Drupal site.</p>' . '<p>Upon import, your pages, navigation structure, and content will be instantly ready in your CMS.</p>' . '<p>Pick a XML file to upload and click Import.</p>',
-            '#upload_validators'=>$file_validator,
+            '#upload_validators' => $file_validator
         );
         
         $form['submit'] = array(
@@ -36,20 +43,22 @@ class UploadForm extends FormBase
     }
 
     public function validateForm(array &$form, FormStateInterface $form_state)
-    {
-        if (! $this->file) {
-            $form_state->setErrorByName('slickplan', 'File not found');
-        }
-    }
+    {}
 
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
-        $file = $form_state->getValue('file');
+        $xml = $form_state->getValue('slickplan');
+        $file = file_load($xml[0]);
+        $data = file_get_contents($file->getFileUri());
         
-        if(isset($file) && is_file($file)) {
-            drupal_set_message(file_get_contents($file));
-        } else {
-            drupal_set_message("Problem z plikiem!");
+        if (isset($data) && ! empty($data)) {
+            
+            try {
+                $slickplan = new SlickplanController();
+                $data = $slickplan->parseSlickplanXml($data);
+            } catch (Exception $ex) {
+                drupal_set_message($e->getMessage(), 'error');
+            }
         }
     }
 }
