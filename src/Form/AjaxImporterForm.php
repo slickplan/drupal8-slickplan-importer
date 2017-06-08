f8ead872cbfeb5eda24627d9a901f71e3ed87ebb
lukaszawmous <lukasz@awmo.us>
2017-06-08 16:57:13 +0200

    Create a Ajax import form, for more file.

---
+++ src/Form/AjaxImporterForm.php
@@ -0,0 +1,40 @@
+<?php
+namespace Drupal\slickplan\Form;
+
+use Drupal;
+use Drupal\file\Entity\File;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\slickplan\Controller\SlickplanController;
+
+class AjaxImporterForm extends FormBase
+{
+
+    public function getFormId()
+    {
+        return 'ajax_importer_forms';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        
+        $xml = Drupal::state()->get('slickplan_importer');
+        
+        $slickplanToolController = new SlickplanToolController();
+        $slickplanToolController->checkRequiredData($xml, 'ajax_importer');
+        
+        return $form;
+    }
+
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+        
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        
+    }
+}
+
+
