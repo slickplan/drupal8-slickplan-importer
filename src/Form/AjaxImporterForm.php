ccd36fd061d4f6e8bb303d2db904ad710fe0fe85
lukaszawmous <lukasz@awmo.us>
2017-06-09 16:25:44 +0200

    Create a ajax_import pages.

--- src/Form/AjaxImporterForm.php
+++ src/Form/AjaxImporterForm.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Drupal\slickplan\Form;
 
 use Drupal;
@@ -7,9 +6,11 @@ use Drupal\file\Entity\File;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\slickplan\Controller\SlickplanController;
+use Drupal\slickplan\Controller\SlickplanToolController;
 
 class AjaxImporterForm extends FormBase
 {
+
     public function getFormId()
     {
         return 'ajax_importer_forms';
@@ -18,63 +19,100 @@ class AjaxImporterForm extends FormBase
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $xml = Drupal::state()->get('slickplan_importer');
-
+        
         $slickplanToolController = new SlickplanToolController();
         $slickplanToolController->checkRequiredData($xml, 'ajax_importer');
-
-        drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(
-            'type' => 'external',
-        ));
-
-        $slickplan = new Slickplan_Importer;
-
-        $path = drupal_get_path('module', 'slickplan_importer');
-        drupal_add_js(
-            'window.SLICKPLAN_JSON = ' . json_encode($xml['sitemap']) . ';'
-            ."\n"
-            . 'window.SLICKPLAN_HTML = "' . str_replace('"', '\"', $slickplan->getSummaryRow(array(
-                'title' => '{title}',
-                'loading' => 1,
-            )))
-            . '";'
-        ,'inline');
-        drupal_add_js($path . '/js/ajax_importer.js');
-
+        
         $form['page_header'] = array(
             '#markup' => '<h2>Importing Pages&hellip;</h2>',
+            '#attached' => [
+                'library' => 'slickplan/awesome'
+            ]
         );
-
+        
+        $form['slickplan_json'] = array(
+            '#type' => 'hidden',
+            '#value' => json_encode($xml['sitemap'])
+        );
+        
+        $slickplan = new SlickplanController();
+        
+        $form['slickplan_html'] = array(
+            '#type' => 'hidden',
+            '#value' => $slickplan->getSummaryRow(array(
+                'title' => '{title}',
+                'loading' => 1
+            ))
+        );
+        
+        $form['page_header'] = array(
+            '#markup' => '<h2>Importing Pages&hellip;</h2>'
+        );
+        
         $form['message'] = array(
-            '#markup' => '<p style="display: none" class="slickplan-show-summary">Pages have been imported. Thank you for using '
-                . '<a href="http://slickplan.com/" target="_blank">Slickplan</a> Importer.</p>',
+            '#markup' => '<p style="display: none" class="slickplan-show-summary">Pages have been imported. Thank you for using ' . '<a href="http://slickplan.com/" target="_blank">Slickplan</a> Importer.</p>'
         );
-
+        
         $form['progress'] = array(
-            '#markup' => theme_progress_bar(array(
-                'percent' => 0,
-                'message' => '',
-            )),
+            '#type' => 'inline_template',
+            '#template' => '
+                <div class="progress">
+                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="{{ complete }}"
+                aria-valuemin="0" aria-valuemax="100" style="width:{{ complete }}%">
+                {{ complete }}% Complete (success)
+                </div>
+                </div>',
+            '#context' => array(
+                'complete' => '0'
+            ),
+            '#attached' => array(
+                'library' => 'slickplan/multifile'
+            ),
+            '#ajax' => array(
+                'callback' => array(
+                    $this,
+                    'getSummaryPages'
+                ),
+                'effect' => 'fade',
+                'event' => 'change',
+                'progress' => array(
+                    'type' => 'bar',
+                    'message' => NULL
+                )
+            )
         );
-
+        
+        // $form['progress'] = array(
+        // '#markup' => theme_progress_bar(array(
+        // 'percent' => 0,
+        // 'message' => '',
+        // )),
+        // );
+        
         $form['summary'] = array(
             '#markup' => '<p><hr /></p><div class="slickplan-summary"></div><p><hr /></p>',
+            '#attached' => [
+                'library' => 'slickplan/awesome'
+            ]
+        
         );
-
+        
         $form['submit'] = array(
             '#type' => 'link',
             '#href' => 'admin/content',
             '#title' => 'See all pages',
             '#attributes' => array(
                 'style' => 'display: none',
-                'class' => 'slickplan-show-summary',
-            ),
+                'class' => 'slickplan-show-summary'
+            )
         );
-
+        
         return $form;
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
+        exit();
     }
 }
 
--- src/Form/SummaryForm.php
+++ src/Form/SummaryForm.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Drupal\slickplan\Form;
 
 use Drupal;
@@ -10,6 +9,7 @@ use Drupal\slickplan\Controller\SlickplanToolController;
 
 class SummaryForm extends FormBase
 {
+
     public function getFormId()
     {
         return 'summary_forms';
@@ -18,18 +18,18 @@ class SummaryForm extends FormBase
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $xml = Drupal::state()->get('slickplan_importer');
-
+        
         $slickplanToolController = new SlickplanToolController();
         $slickplanToolController->checkRequiredData($xml, 'options');
-
+        
         $form['page_header'] = array(
             '#markup' => '<h2>Success!</h2>'
         );
-
+        
         $form['message'] = array(
             '#markup' => '<p>Pages have been imported. Thank you for using ' . '<a href="http://slickplan.com/" target="_blank">Slickplan</a> Importer.</p>'
         );
-
+        
         if (isset($xml['summary']) and is_array($xml['summary'])) {
             $html = '';
             $slickplan = new SlickplanController();
@@ -38,25 +38,27 @@ class SummaryForm extends FormBase
             }
             if ($html) {
                 $form['summary'] = array(
-                    '#markup' => '<p><hr /></p>' . $html . '<p><hr /></p>'
+                    '#markup' => '<p><hr /></p>' . $html . '<p><hr /></p>',
+                    '#attached' => [
+                        'library' => 'slickplan/awesome'
+                    ]
                 );
             }
         }
-
+        
         $form['submit'] = array(
             '#type' => 'link',
             '#href' => 'admin/content',
             '#title' => 'See all pages'
         );
-
+        
         Drupal::state()->delete('slickplan_importer');
-
+        
         return $form;
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-    }
+    {}
 }
 
 
